b52c6b4d1de87325ee56e273b6df93aadc84fc0d
Mariyam-Aqsa-SangamOne <mariyamaqsa.sangamone@gmail.com>
2026-05-16 20:03:04 +0530

    Expense Tracker

---
+++ Expense_Tracker.py
@@ -0,0 +1,27 @@
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host="139.162.60.69",
+    port=3306,
+    user="user1",
+    password="Navatar2024!",
+    database="expense_tracker"
+)
+
+cursor = conn.cursor()
+
+cursor.execute("SELECT * FROM expenses;")
+
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+print()
+
+cursor.execute("SELECT SUM(amount) FROM expenses")
+result = cursor.fetchone()
+
+print("Total expense for May 2026:", result[0])
+
+conn.close()
