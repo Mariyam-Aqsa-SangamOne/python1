aa4c24681e025d8b4c6f08852e78ac433c94af9b
Mariyam-Aqsa-SangamOne <mariyamaqsa.sangamone@gmail.com>
2026-05-17 21:58:58 +0530

    Expense_Tracker

--- Expense_Tracker.py
+++ Expense_Tracker.py
@@ -1,23 +1,24 @@
+import os
 import mysql.connector
+
+db_password = os.getenv("DB_PASSWORD")
 
 conn = mysql.connector.connect(
     host="139.162.60.69",
     port=3306,
     user="user1",
-    password="",
+    password=db_password,
     database="expense_tracker"
 )
 
 cursor = conn.cursor()
 
-cursor.execute("SELECT * FROM expenses;")
-
+cursor.execute("SELECT * FROM expenses")
 rows = cursor.fetchall()
 
+print("Expense Table Records:")
 for row in rows:
     print(row)
-
-print()
 
 cursor.execute("SELECT SUM(amount) FROM expenses")
 result = cursor.fetchone()
